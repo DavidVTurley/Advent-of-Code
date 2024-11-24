c009a0e49c04bc0d3200c8126e1073764f413df9
david turley <david.turley@live.nl>
2024-11-24 17:58:41 +0100

    Added 2015 day 1

--- Advent of Code/2015/Day1.cs
+++ Advent of Code/2015/Day1.cs
@@ -10,18 +10,40 @@ internal class Day1 : IDay
     public int Year { get; } = 2015;
     public int Day { get; } = 1;
 
+    private string _challengeText = string.Empty;
+
     public string Part1()
     {
-        throw new NotImplementedException();
+        int count = 0;
+        foreach (var c in _challengeText.AsSpan())
+        {
+            if (c.Equals('(')) count++;
+            else if (c.Equals(')')) count--;
+        }
+        return count.ToString();
     }
 
     public string Part2()
     {
-        throw new NotImplementedException();
+        int count = 0;
+        for (int i = 0; i < _challengeText.Length; i++)
+        {
+            char c = _challengeText[i];
+            if (c.Equals('('))
+                count++;
+            else if (c.Equals(')'))
+                count--;
+
+            if(count < 0) 
+                return (i+1).ToString();
+        }
+        return "Error";
     }
 
     public void Setup(string task)
     {
-        throw new NotImplementedException();
+        _challengeText = task;
     }
+
+
 }
